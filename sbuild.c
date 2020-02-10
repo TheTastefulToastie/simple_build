2a1b2a5c1dc5f34227ada9935f375a1ce962dde9
TheTastefulToastie <tastefultoastie@gmail.com>
2020-02-10 07:47:06 +0000

    Fix Windows line endings, print executed cmd, flush stdout

--- sbuild.c
+++ sbuild.c
@@ -1,17 +1,27 @@
 // @Compile gcc -std=c99 sbuild.c -o sbuild
 // @Compile gcc -std=c99 sbuild.c -o sbuild && sudo cp sbuild /usr/bin
 
+#include <unistd.h>
+#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+static inline
 char * read_first_line(char *path) {
-  const int max_len = 1024;
-  char *line = malloc(max_len);
   FILE *fp = fopen(path, "r");
+  if (fp == NULL) {
+    fprintf(stderr, "Failed to open file \"%s\".", path);
+    return NULL;
+  }
+  const int max_len = 1024;
+  char *line = calloc(1, max_len);
   fgets(line, max_len, fp);
   fclose(fp);
-  line[strlen(line) - 1] = 0; // Remove newline
+  // TODO: Handle windows line endings
+  size_t len = strlen(line);
+  line[len - 1] = 0; // Remove newline
+  if (line[len - 2] == '\r') line[len - 2] = 0;
   return line;
 }
 
@@ -21,11 +31,15 @@ int main(int argc, char **argv) {
     return 1;
   }
   char *first_line = read_first_line(argv[1]);
-  const char *tag = "@Compile";
-  const int tag_len = strlen(tag);
-  char *tag_start = strstr(first_line, tag);
-  if (tag_start)
-    system(tag_start + tag_len);
+  if (first_line == NULL) return 1;
+  const char *tag = "@Compile ";
+  char *tag_end = strstr(first_line, tag) + strlen(tag);
+  int return_value = 0;
+  if (tag) {
+    printf("> %s\n", tag_end);
+    fflush(stdout);
+    return_value = system(tag_end);
+  }
   free(first_line);
-  return 0;
+  return return_value;
 }
